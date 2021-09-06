7ca96dc9cf1f1fa38b5dcdf70454ae8f5c7496b7
Sashank Balusu <sashankbalusu@gmail.com>
2021-09-06 01:40:51 -0700

    test

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,27 @@
 const auth = firebase.auth()
 const logInBtn = document.getElementById('logInBtn')
-
+const storage = getStorage()
 auth.onAuthStateChanged((user) => {
  console.log("logged in")
  console.log(user.uid)
- firebase.database().ref(user.uid).on('value', function(snapshot){
-   console.log(snapshot.val().Link)
-   document.getElementById("myvid").src = snapshot.val().Link
- })
+ let useruid = user.uid
+//  firebase.storage().ref(user.uid).on('value', function(snapshot){
+//    console.log(snapshot.val().Link)
+//    document.getElementById("myvid").src = snapshot.val().Link
+//  })
+
+const listRef = storageRef.child(useruid.toString())
+
+listRef.listAll()
+.then((res) => {
+  res.prefixes.forEach((folderRef) => {
+
+  })
+  res.items.forEach((itemRef) => {
+    console.log(itemRef)
+    document.getElementById("myvid").src = itemRef
+  })
+})
 
 })
 
